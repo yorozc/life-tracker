0e1fe9b74361a88278a99f23383d08cd00f54f71
yorozc <143294600+yorozc@users.noreply.github.com>
2025-06-06 23:06:11 -0700

    created task table

--- src/db.py
+++ src/db.py
@@ -2,3 +2,16 @@ import sqlite3
 
 conn = sqlite3.connect('src/task.db')
 
+c = conn.cursor() #used to run sql commands
+
+#create task table
+c.execute("""create table tasks (
+            taskName text,
+            description text,
+            checked, integer,
+            data, blob
+          )""")
+
+conn.commit()
+
+conn.close()
